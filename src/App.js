4757d62b9549765cca5231c66ac3107a8ea4d6e8
rada102888 <47612081+rada102888@users.noreply.github.com>
2024-01-24 16:36:23 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { LoginSocialFacebook } from "reactjs-social-login";
+import { FacebookLoginButton } from "react-social-login-buttons";
+
+function App() {
+  const [profile, setProfile] = useState(null);
+
+  return (
+    <div>
+      {!profile ? (
+        <LoginSocialFacebook
+          appId="902293381534153"
+          onResolve={(response) => {
+            console.log(response);
+            setProfile(response.data);
+          }}
+          onReject={(error) => {
+            console.log(error);
+          }}
+        >
+          <FacebookLoginButton />
+        </LoginSocialFacebook>
+      ) : (
+        ""
+      )}
+
+      {profile ? (
+        <div>
+          <h1>{profile.name}</h1>
+          <img src={profile.picture.data.url} />
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+
+export default App;
